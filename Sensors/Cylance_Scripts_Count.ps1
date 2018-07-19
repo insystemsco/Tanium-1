405624d0b6b09f7503c04cb9a9c52d2bfc342fb3
PoppaShell <PoppaShell@ProtonMail.com>
2018-07-19 16:48:49 -0500

    Create Cylance_Scripts_Count.ps1
    Tanium Sensor for Cylance to pull the Scripts Count from the Status.json file located in %ProgramData%\Cylance\".

---
+++ Sensors/Cylance_Scripts_Count.ps1
@@ -0,0 +1,12 @@
+Add-Type -Assembly System.Web.Extensions
+$StatusJSON="$env:ProgramData\Cylance\Status\Status.json"
+$StatusJSONExists = [System.IO.File]::Exists("$StatusJSON")
+
+If ($StatusJSONExists -eq $False){
+    Write "File Not Found"
+    }
+Else {
+    $StatusJSONContent=Get-Content $StatusJSON
+    $Output=(New-Object System.Web.Script.Serialization.JavaScriptSerializer).DeserializeObject($StatusJSONContent)
+    $Output.Scripts.count
+    }
